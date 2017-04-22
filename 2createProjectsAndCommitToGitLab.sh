764f13fe73d0f5332d1a6a0fca4f5e02c9ba63e5
Hedi Nasr <h.nsr69@gmail.com>
2017-04-22 09:14:02 +0000

    gitlabcli

--- 2createProjectsAndCommitToGitLab.sh
+++ 2createProjectsAndCommitToGitLab.sh
@@ -1,23 +1,23 @@
 #! /bin/bash
 
 # use same credentials for git as for gitlab user
-
-echo "precondition: create project 'hello-world-app' in gitlab"
-echo "The default user is 'root' and the pw is shown above"
-
-ip="$(ifconfig | grep -A 1 'eth0' | tail -1 | cut -d ':' -f 2 | cut -d ' ' -f 1)"
+ip=$(/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)
 TARGETFOLDER=~/Devel
 GITLAB_USER=root
 GITLAB_PW=12345678
-GITLAB_URL=$ip:10080
+#GITLAB_URL=$ip:10080
+GITLAB_URL=gitlab
 
-_TOKEN="curl http://$ip:10080/api/v3/session --data 'login=$GITLAB_USER&password=$GITLAB_PW' | jq '.private_token'"
-TOKEN=$(eval $_TOKEN)
+_TOKEN="curl -s http://$ip:10080/api/v3/session --data 'login=$GITLAB_USER&password=$GITLAB_PW' | jq '.private_token'"
+TOKEN=$(eval $_TOKEN | tr -d '"')
 
-docker run -it veden/gitlab-cli --help
+docker run -it -e GITLAB_API_ENDPOINT=http://$GITLAB_URL/api/v4 \
+               -e GITLAB_API_PRIVATE_TOKEN=$TOKEN \
+               --net puppet_default \
+               test/gitlabcli gitlab users --json
 
-git config --global user.name "Administrator"
-git config --global user.email "admin@example.com"
+#git config --global user.name "Administrator"
+#git config --global user.email "admin@example.com"
 
 createProjectAndCommitToGitLab(){
 	local projectName=$1
@@ -48,4 +48,4 @@ createProjectAndCommitToGitLab(){
 	cd $currentPath
 }
 
-createProjectAndCommitToGitLab code
+#createProjectAndCommitToGitLab code
