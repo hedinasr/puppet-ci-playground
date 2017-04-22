9ba7b6065ae62983a964909154d3a77576372c5e
Hedi Nasr <h.nsr69@gmail.com>
2017-04-22 10:34:37 +0200

    update script

--- 2createProjectsAndCommitToGitLab.sh
+++ 2createProjectsAndCommitToGitLab.sh
@@ -5,10 +5,16 @@
 echo "precondition: create project 'hello-world-app' in gitlab"
 echo "The default user is 'root' and the pw is shown above"
 
+ip="$(ifconfig | grep -A 1 'eth0' | tail -1 | cut -d ':' -f 2 | cut -d ' ' -f 1)"
 TARGETFOLDER=~/Devel
 GITLAB_USER=root
 GITLAB_PW=12345678
-GITLAB_ADDR=10.0.69.3:10080
+GITLAB_URL=$ip:10080
+
+_TOKEN="curl http://$ip:10080/api/v3/session --data 'login=$GITLAB_USER&password=$GITLAB_PW' | jq '.private_token'"
+TOKEN=$(eval $_TOKEN)
+
+docker run -it veden/gitlab-cli --help
 
 git config --global user.name "Administrator"
 git config --global user.email "admin@example.com"
@@ -21,7 +27,7 @@ createProjectAndCommitToGitLab(){
 	rm -fr $targetPath
 
 	echo "--- Cloning..."
-	git clone http://$GITLAB_USER:$GITLAB_PW@$GITLAB_ADDR/root/$projectName.git $targetPath
+	git clone http://$GITLAB_USER:$GITLAB_PW@$GITLAB_URL/root/$projectName.git $targetPath
 	#git config user.name $GITLAB_USER #don't use --global!
 	git config credential.helper cache #caches password for 15 min
 
