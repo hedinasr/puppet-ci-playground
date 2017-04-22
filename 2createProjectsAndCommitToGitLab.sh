f6f81af439e72127c912591caf50dc0b3d300588
Hedi Nasr <h.nsr69@gmail.com>
2017-04-22 15:09:10 +0200

    createProject gitlab

--- 2createProjectsAndCommitToGitLab.sh
+++ 2createProjectsAndCommitToGitLab.sh
@@ -5,19 +5,21 @@ ip=$(/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)
 TARGETFOLDER=~/Devel
 GITLAB_USER=root
 GITLAB_PW=12345678
-#GITLAB_URL=$ip:10080
+GITLAB_ADDR=$ip:10080
 GITLAB_URL=gitlab
 
 _TOKEN="curl -s http://$ip:10080/api/v3/session --data 'login=$GITLAB_USER&password=$GITLAB_PW' | jq '.private_token'"
 TOKEN=$(eval $_TOKEN | tr -d '"')
 
-docker run -it -e GITLAB_API_ENDPOINT=http://$GITLAB_URL/api/v4 \
-               -e GITLAB_API_PRIVATE_TOKEN=$TOKEN \
-               --net puppet_default \
-               test/gitlabcli gitlab users --json
+git config --global user.name "Administrator"
+git config --global user.email "admin@example.com"
 
-#git config --global user.name "Administrator"
-#git config --global user.email "admin@example.com"
+createProject() {
+    docker run -it -e GITLAB_API_ENDPOINT=http://$GITLAB_URL/api/v4 \
+                   -e GITLAB_API_PRIVATE_TOKEN=$TOKEN \
+                   --net puppet_default \
+                   test/gitlab-cli gitlab create_project $1
+}
 
 createProjectAndCommitToGitLab(){
 	local projectName=$1
@@ -27,16 +29,12 @@ createProjectAndCommitToGitLab(){
 	rm -fr $targetPath
 
 	echo "--- Cloning..."
-	git clone http://$GITLAB_USER:$GITLAB_PW@$GITLAB_URL/root/$projectName.git $targetPath
+	git clone http://$GITLAB_USER:$GITLAB_PW@$GITLAB_ADDR/root/$projectName.git $targetPath
 	#git config user.name $GITLAB_USER #don't use --global!
 	git config credential.helper cache #caches password for 15 min
 
 	echo "--- Copying..."
 	cp -r $projectName $TARGETFOLDER
-	rm -fr $targetPath/target
-	rm -fr $targetPath/.settings
-	rm -fr $targetPath/.classpath
-	rm -fr $targetPath/.project
 
 	echo "--- Committing..."
 	local currentPath=`pwd`;
@@ -48,4 +46,8 @@ createProjectAndCommitToGitLab(){
 	cd $currentPath
 }
 
-#createProjectAndCommitToGitLab code
+createProject code
+createProjectAndCommitToGitLab code
+
+createProject my-module
+createProjectAndCommitToGitLab my-module
